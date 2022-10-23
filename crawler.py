8fd4251a31caeb8fa30164e4acb28a7c210250c4
Minchan <minchan06@gmail.com>
2022-10-24 02:23:52 +0900

    Improve crawling speed
    shorten rendering time by skipping images.
    changed chrome invisible.

--- crawler.py
+++ crawler.py
@@ -5,15 +5,23 @@ import time
 from webdriver_manager.chrome import ChromeDriverManager
 
 base_url = 'http://place.map.kakao.com/'
+service = Service(ChromeDriverManager().install())
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("--blink-setting=imagesEnable=false")
 
 def extract_menu(id_list):
     full_menu = dict()  # 모든 가게의 메뉴
     service = Service(ChromeDriverManager().install())
-    browser = webdriver.Chrome(service=service)
+    browser = webdriver.Chrome(service=service, options = options)
+
     for id in id_list:
         menu_list = dict()
         browser.get(base_url + id)
-        time.sleep(1)
+        time.sleep(0.3)
         soup = BeautifulSoup(browser.execute_script('return document.body.innerHTML'), 'html.parser')
         menu = soup.find("ul", class_="list_menu")
         if not menu:
@@ -28,6 +36,8 @@ def extract_menu(id_list):
             except :
                 continue
             name = coffee.find("span", class_="loss_word").string
+            if name is None:
+                name = str(coffee.find("span", class_="loss_word")).split('</span>')[-2]
             menu_list[name] = price
         full_menu[id] = menu_list
     browser.quit()
